02c7030bdf2362f4a5182873afd0b43923a0f8f9
william <sunsaying@gmail.com>
2020-10-14 09:32:40 +1000

    removed markdown

--- app/Main.js
+++ app/Main.js
@@ -4,7 +4,7 @@ import { useImmerReducer } from "use-immer"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import { CSSTransition } from "react-transition-group"
 import Axios from "axios"
-Axios.defaults.baseURL = process.env.BACKENDURL || "http://localhost:3000"
+Axios.defaults.baseURL = process.env.BACKENDURL || "https://postandshare.herokuapp.com"
 
 import StateContext from "./StateContext"
 import DispatchContext from "./DispatchContext"

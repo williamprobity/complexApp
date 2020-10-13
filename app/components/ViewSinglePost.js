84fc72fe7cb9045b1da5b627fddc252c3a25dcdd
william <sunsaying@gmail.com>
2020-10-13 14:24:59 +1000

    changed

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,13 +1,13 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useEffect, useState, useContext } from "react"
 import Page from "./Page"
 import { useParams, Link, withRouter } from "react-router-dom"
 import Axios from "axios"
 import LoadingDotsIcon from "./LoadingDotsIcon"
 import ReactMarkdown from "react-markdown"
-import ReactTooltip from 'react-tooltip'
-import NotFound from './NotFound'
-import StateContext from '../StateContext'
-import DispatchContext from '../DispatchContext'
+import ReactTooltip from "react-tooltip"
+import NotFound from "./NotFound"
+import StateContext from "../StateContext"
+import DispatchContext from "../DispatchContext"
 
 function ViewSinglePost(props) {
   const appState = useContext(StateContext)
@@ -33,9 +33,9 @@ function ViewSinglePost(props) {
       ourRequest.cancel()
     }
   }, [id])
-  
-  if(!isLoading && !post){
-    return <NotFound/>
+
+  if (!isLoading && !post) {
+    return <NotFound />
   }
 
   if (isLoading)
@@ -47,27 +47,27 @@ function ViewSinglePost(props) {
 
   const date = new Date(post.createdDate)
   const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
-  
-  function isOwner(){
-    if(appState.loggedIn){
+
+  function isOwner() {
+    if (appState.loggedIn) {
       return appState.user.username == post.author.username
     }
     return false
   }
 
-async function deleteHandler () {
+  async function deleteHandler() {
     const areYouSure = window.confirm("Do you really want to delete this post?")
-    if(areYouSure){
-      try{
-        const response = await Axios({url: `/post/${id}`, data: {token: appState.user.token}, method: 'delete'})
+    if (areYouSure) {
+      try {
+        const response = await Axios.delete(`/post/${id}`, { data: { token: appState.user.token } })
+        if (response.data == "Success") {
+          // 1. display a flash message
+          appDispatch({ type: "flashMessage", value: "Post was successfully deleted." })
 
-        if(response.data == "Success"){
-          //1. display a flash message
-          appDispatch({type: "flashMessage", value: "Post was successfully deleted."})
-          //2. redirect back to the current user's profile
+          // 2. redirect back to the current user's profile
           props.history.push(`/profile/${appState.user.username}`)
         }
-      }catch (e) {
+      } catch (e) {
         console.log("There was a problem.")
       }
     }
@@ -78,17 +78,16 @@ async function deleteHandler () {
       <div className="d-flex justify-content-between">
         <h2>{post.title}</h2>
         {isOwner() && (
-        <span className="pt-2">
-          <Link to={`/post/${post._id}/edit`} data-tip="Edit" data-for="edit" className="text-primary mr-2">
-            <i className="fas fa-edit"></i>
-          </Link>
-          <ReactTooltip id="edit" className="custom-tooltip"/>
-          {" "}
-          <a onClick={deleteHandler} data-tip="Delete" data-for="delete" className="delete-post-button text-danger">
-            <i className="fas fa-trash"></i>
-          </a>
-          <ReactTooltip id="delete" className="custom-tooltip" />
-        </span>
+          <span className="pt-2">
+            <Link to={`/post/${post._id}/edit`} data-tip="Edit" data-for="edit" className="text-primary mr-2">
+              <i className="fas fa-edit"></i>
+            </Link>
+            <ReactTooltip id="edit" className="custom-tooltip" />{" "}
+            <a onClick={deleteHandler} data-tip="Delete" data-for="delete" className="delete-post-button text-danger">
+              <i className="fas fa-trash"></i>
+            </a>
+            <ReactTooltip id="delete" className="custom-tooltip" />
+          </span>
         )}
       </div>
 
